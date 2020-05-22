945c4792e539c5fe46a3a1fdf362e464c5a95469
Scott  Keplinger <skeplin17@gmail.com>
2020-05-22 19:29:34 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,14 +1,13 @@
 # Write your code here
 class GetRequester
 
-URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
 def initialize(url)
-  URL = url
+  @url = url
 end
 
   def get_response_body
-    uri = URI.parse(URL)
+    uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
   end
